35128eb06b81f83c34f423261ba7a9030a6994ce
VietHungHoang <viethunghoang1508@gmail.com>
2025-06-07 21:51:01 +0700

    feat: userservice - Add settings, models and migrate

--- userservice/users_app/models.py
+++ userservice/users_app/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'roles'
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, related_name='users')
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'users'
